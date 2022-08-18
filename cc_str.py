9f9e65dab5b6575db7e3a5c366f8e10e1d0664ab
dannis999 <39402843+dannis999@users.noreply.github.com>
2022-08-18 15:34:15 +0800

    添加日志相关

--- cc_str.py
+++ cc_str.py
@@ -1,5 +1,7 @@
 '字符串相关'
 
+import sys,time,datetime
+
 UNITS = 'KMGTPEZYBND'
 
 def strsize(x):
@@ -30,12 +32,51 @@ def strtime(a):
         du = us[n]
     return '%.8g %s' % (a,du)
 
+def log_info(s:str,f=sys.stdout):
+    dt = datetime.datetime.now()
+    ts = dt.isoformat(' ')
+    msg = f'[{ts}]{s}\n'
+    f.write(msg)
+    f.flush()
+
+class prog_logger:
+    '进度提示器'
+    def __init__(self):
+        self.start()
+
+    def start(self):
+        self.t = time.time()
+
+    def finish(self):
+        log_info('finish')
+
+    def log(self,i,n,delay=300):
+        dt = time.time()
+        if dt < self.t + delay:return
+        self.t = dt
+        log_info(f'{i}/{n}')
+
+class prog_logger2(prog_logger):
+    def __init__(self):
+        super().__init__()
+        self.n = 0
+
+    def log(self,dn,delay=300):
+        self.n += dn
+        dt = time.time()
+        if dt < self.t + delay:return
+        self.t = dt
+        log_info(str(self.n))
+
+    def finish(self):
+        log_info(f'finish {self.n}')
+
 def _test():
     xs = (0,1,100,10**7,123<<30,907<<30,1008<<30,234<<180)
     for x in xs:
-        print(strsize(x))
+        log_info(strsize(x))
     for x in xs:
-        print(strtime(x))
+        log_info(strtime(x))
 
 if __name__=='__main__':
     _test()
